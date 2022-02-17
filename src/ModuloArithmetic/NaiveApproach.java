504a897e6cc64747a8606dec504e5a6147bd5e5b
Deepak <dk8400779@gmail.com>
2022-02-17 15:56:33 +0530

    Initial commit

---
+++ src/ModuloArithmetic/NaiveApproach.java
@@ -0,0 +1,22 @@
+package ModuloArithmetic;
+
+import java.util.Scanner;
+
+public class NaiveApproach {
+    // For Example Calculate 3^10
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc-- > 0) {
+            System.out.print("Enter Base: ");
+            int base = sc.nextInt();
+            System.out.print("Enter Exponent: ");
+            int exp = sc.nextInt();
+            int ans = 1;
+            for (int i = 1; i <= exp; i++) {
+                ans *= base;
+            }
+            System.out.println(ans);
+        }
+    }
+}
