531717f7ce9c09ca7f642f49fe8f9c374ba2f1ad
Deepak <dk8400779@gmail.com>
2022-02-18 00:04:06 +0530

    Initial commit

---
+++ src/ModuloArithmetic/ModuloProperties.java
@@ -0,0 +1,15 @@
+package ModuloArithmetic;
+
+public class ModuloProperties {
+    /*
+    Modulo Properties:
+      1. (a + b) % m = ((a % m) + (b % m)) % m
+      2. (a - b) % m = ((a % m) - (b % m)) % m
+      3. (a * b) % m = ((a % m) * (b % m)) % m
+      4. (a / b) % m = ((a % m) / (b % m)) % m
+                     = ((a % m) * (b^-1 % m)) % m
+                   --> b^-1 : Multiplicative Modulo Inverse
+      5. (a % m) % m = a % m
+      6. m^x % m = 0 For All x Belongs to Z+ (Positive integer)
+     */
+}
