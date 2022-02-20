007bd8d369d3d48b8d2da310eea088ad9018ad02
Deepak <dk8400779@gmail.com>
2022-02-20 23:39:05 +0530

    Initial commit

--- src/AdhocProblems/Equalize.java
+++ src/AdhocProblems/Equalize.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Equalize {
     public static void adhoc(String s, String t){
         int cost = 0;
-        for(int i = 0; i<s.length(); i++){
+        for(int i = 0; i<s.length(); ){
             if(s.charAt(i) != t.charAt(i)){
                 // Either Swap or Flip
                 if(i+1 < s.length() && s.charAt(i) != s.charAt(i+1) && s.charAt(i+1) != t.charAt(i+1)){
